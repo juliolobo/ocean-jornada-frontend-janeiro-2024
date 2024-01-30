ea8032f582dd0111f3faec0243525c4cbc1cd33f
Julio Lobo <juliolobo@lobohost.com.br>
2024-01-30 12:49:40 +0000

    Criação e estilização das tags

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,13 @@
+import Tag from "../Tag/Tag"
+
 function Card() {
     return (
       <>
         <div className='card'>
           <h2>Rick Sanchez</h2>
+          <div className="tags">
+            <Tag />
+          </div>
           <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
         </div>
       </>
---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+function Tag() {
+    return (
+        <>
+        <div className="tag">Status: Vivo</div>
+        <div className="tag">Espécie: Humana</div>
+        <div className="tag">Origem: Terra C-137</div>
+        </>
+    )
+}
+
+export default Tag
