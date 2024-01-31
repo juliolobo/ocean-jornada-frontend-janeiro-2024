48a7f19b5af53b27d8dcb5a90ee4e6503a44d229
Julio Lobo <juliolobo@lobohost.com.br>
2024-01-31 15:01:36 +0000

    Carregando e usando Dados da Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,25 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-  const item1 = {
-    name:'Rick Sanchez', 
-    image:'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-    tags: ['Status: Vivo', 'Espécie: Humana', 'Origem: Terra C-137']
-  }
-  const item2 = {
-    name:'Morty Smith', 
-    image:'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
-    tags: ['Status: Vivo', 'Espécie: Humana', 'Origem: Terra C-137']
-  }
-  const item3 = {
-    name:'Summer Smith', 
-    image:'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
-    tags: ['Status: Vivo', 'Espécie: Humana', 'Origem: Terra C-137']
-  }
-  const item4 = {
-    name:'Beth Smith', 
-    image:'https://rickandmortyapi.com/api/character/avatar/4.jpeg',
-    tags: ['Status: Vivo', 'Espécie: Humana', 'Origem: Terra C-137']
+  const [itens, setItens] = useState([])
+
+  async function carregarDadosApi() {
+    const apiUrl = 'https://rickandmortyapi.com/api/character/'
+    const response = await fetch(apiUrl)
+    const body = await response.json()
+    const results = body.results
+
+    setItens(results)
   }
 
-  const itens = [item1, item2, item3, item4]
+  useEffect(function () {
+    carregarDadosApi()
+  }, [])
+
+  console.log(itens)
 
   return (
     <>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,13 +2,14 @@ import Tag from "../Tag/Tag"
 
 function Card(props) {
   const item = props.item
+
+  const tags = [item.status, item.species, item.origin.name]
     return (
       <>
         <div className='card'>
           <h2>{item.name}</h2>
           <div className="tags">
-            <Tag tag={item.tags} />
-            {/* <Tag /> */}
+            <Tag tag={tags} />
           </div>
           <img src={item.image} />
         </div>
